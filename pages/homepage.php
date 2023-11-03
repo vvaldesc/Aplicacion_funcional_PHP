48c3f465ae0fc0a414c8e6046f4f1f180b5beba7
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-03 16:41:24 +0100

    Planteamiento y comentarios

---
+++ pages/homepage.php
@@ -0,0 +1,7 @@
+<?php
+
+//Aquí ya he iniciado sesión
+
+//Habría que plantear si vamos a inyectar el sql en NetBeans (cuenta nota)
+//en ese caso podriamos comprobar aquí si existen las tablas necesarias
+//en caso contrario inyectamos la tabla que faltaa modo de prueba
