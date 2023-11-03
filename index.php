83e0eb2d421acebac46eee8f9a256f27826bf029
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-03 16:27:10 +0100

    Formulario provisional inicial

--- index.php
+++ index.php
@@ -6,11 +6,32 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
+        <!<!-- container -->
+        <div style="width: 1000px; margin: auto">
+        
+        <?php //comprobar credenciales y si no, error ?>
+
+        <form method="POST" action=<?php $_SERVER["PHP_SELF"] ?>>
+
+            <div class="form-group">
+                <label for="nombre">Usuario</label>
+                <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre">
+            </div>
+            <div class="form-group">
+                <label for="ape">Contraseña</label>
+                <input type="text" class="form-control" id="ape" name="apellidos" placeholder="Apellidos">
+            </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+
+        </form>
+            
+        </div>
+
     </body>
 </html>
