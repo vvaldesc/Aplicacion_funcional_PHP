96f6f2a609b84c78e04eb3ca0173dca890e5d84c
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-05 19:35:43 +0100

    trabajando en PDO

--- pages/conexionPDO.php
+++ pages/conexionPDO.php
@@ -1,18 +1,29 @@
 <?php
 
-$cadena_conexion = 'mysql:dbname=<base de datos>;host=<ip o nombre>';
+function conexionPDO(){
+
+$cadena_conexion = 'mysql:dbname=Concesionario;host=localhost';
 //public PDO::__construct("driver","usr","pass","array_opcional");
+$username = 'usrConcesionario';
+$password = 'pbazEMdm)vf/d43_';
 
 
 
 try {
     $BD=new PDO($cadena_conexion, $username, $password);
+    $BD->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    //Consulta para ver el numero de tablas en concesionario a modo de prueba
+    //$BD->closeCursor();
+    $sql ="SELECT COUNT(*) as tablas_totales from concesionario where table_schema =: concsionario";
+    $stmt = $BD->query($sql);
+    $stmt -> execute();
+    print_r($stmt);
     
-    $BD->closeCursor();
     
     $BD = null;
 } catch (Exception $exc) {
     echo $exc->getMessage();
 }
-
+}
 
