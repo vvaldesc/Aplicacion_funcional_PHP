462472a42a4862f8117dafa067effc4144d93af0
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-05 20:33:23 +0100

    trabajando en PDO (conexion a mysql y primera consulta incluida)

--- pages/conexionPDO.php
+++ pages/conexionPDO.php
@@ -2,7 +2,7 @@
 
 function conexionPDO(){
 
-$cadena_conexion = 'mysql:dbname=Concesionario;host=localhost';
+$cadena_conexion = 'mysql:dbname=concesionario;host=localhost';
 //public PDO::__construct("driver","usr","pass","array_opcional");
 $username = 'usrConcesionario';
 $password = 'pbazEMdm)vf/d43_';
@@ -15,10 +15,18 @@ try {
 
     //Consulta para ver el numero de tablas en concesionario a modo de prueba
     //$BD->closeCursor();
-    $sql ="SELECT COUNT(*) as tablas_totales from concesionario where table_schema =: concsionario";
-    $stmt = $BD->query($sql);
-    $stmt -> execute();
-    print_r($stmt);
+    
+    $sql="SHOW TABLES";
+    $tablas = $BD->query($sql);
+    $instancias = $tablas->fetchAll(PDO::FETCH_COLUMN);
+    
+    foreach ($instancias as $fila){
+        echo "tabla";
+    }
+    
+    //echo count($tablas);
+    
+    echo "Conexion";
     
     
     $BD = null;
