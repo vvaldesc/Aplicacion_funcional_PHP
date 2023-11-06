188d2aeb04a100b1b4addccebb4cdce0cc4369e5
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-06 18:26:21 +0100

    Trabajando en PDO (Funciones CRUD)

--- index.php
+++ index.php
@@ -7,50 +7,45 @@
     </head>
     <body>
         <div class="container mt-4">
-        <!-- container -->
-        
-        <?php
-        include './pages/conexionPDO.php';
-        //comprobar credenciales y token y si no, error
-        //el formulario te llevaría a homepage
-        
-        if ($_SERVER["REQUEST_METHOD"]=="POST") {
-            if(isset($_POST["pass"])){
-                
-                //si usr y pass son correctos...
-                session_start();
-                $_SESSION["usuario"] = $_POST["usr"];
-                header('Location:pages/homepage.php');
-            
-                
-            }else{
-                header('Location:');
+            <!-- container -->
+
+            <?php
+            include './pages/conexionPDO.php';
+            //comprobar credenciales y token y si no, error
+            //el formulario te llevaría a homepage
+
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                if (isset($_POST["pass"])) {
+
+                    //si usr y pass son correctos...
+                    session_start();
+                    $_SESSION["usuario"] = $_POST["usr"];
+                    header('Location:pages/homepage.php');
+                } else {
+                    header('Location:');
+                }
             }
-        }
-        
-        conexionPDO();
-        
-        
-        
-        ?>
-        
-        <h1 class="text-center">Inicio de sesión</h1>
 
-        <form method="POST" class="text-center mt-4 p-4 border d-flex align-items-center flex-column" style="width: 400px; margin: auto" action=<?php $_SERVER["PHP_SELF"] ?>>
-            
-            <div class="form-group m-2" style="width: 300px">
-                <label for="usr">Usuario</label>
-                <input type="text" class="form-control" id="usr" name="usr" placeholder="Usuario">
-            </div>
-            <div class="form-group m-2" style="width: 300px">
-                <label for="pass">Contraseña</label>
-                <input type="text" class="form-control" id="pass" name="pass" placeholder="Contraseña">
-            </div>
+            crearBD();
+            ?>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
+            <h1 class="text-center">Inicio de sesión</h1>
 
-        </form>
-            
+            <form method="POST" class="text-center mt-4 p-4 border d-flex align-items-center flex-column" style="width: 400px; margin: auto" action=<?php $_SERVER["PHP_SELF"] ?>>
+
+                <div class="form-group m-2" style="width: 300px">
+                    <label for="usr">Usuario</label>
+                    <input type="text" class="form-control" id="usr" name="usr" placeholder="Usuario">
+                </div>
+                <div class="form-group m-2" style="width: 300px">
+                    <label for="pass">Contraseña</label>
+                    <input type="text" class="form-control" id="pass" name="pass" placeholder="Contraseña">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+            </form>
+
         </div>
 
     </body>
