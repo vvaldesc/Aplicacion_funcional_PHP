b8bf2aeed79ad05db26977deadf4be68fbf67309
javierdm01 <diazmolinajavier04@gmail.com>
2023-11-13 18:00:22 +0100

    CIFRADO DE CONTRASEÑA

--- index.php
+++ index.php
@@ -13,44 +13,47 @@
             
             include $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/conexionPDO.php';
             include $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/funciones.php';
+            
+            session_start();
+            
+            generaToken($_SESSION['token'],session_id());
 
             //comprobar credenciales y token y si no, error
             //el formulario te llevaría a homepage
 
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                
-                
-                
-                
-                
+
                 if (isset($_POST["pass"]) && isset($_POST["usr"]) ) {
+                    
                     if($_POST["pass"]!='' && $_POST["usr"]!=''){
                         
-                        $BD= conexionPDO();
-                        
-                        $sql="SELECT CONTRASENA FROM USUARIOS WHERE USUARIO IS ".$_POST['usr'];
-                        
-                        $cursorSql = $BD->query($sql);
-                        
-                        // AL SER USUARIO CLAVE UNICA LA PRIMERA CONDICIÓN ES PRÁCTICAMENTE INNECESARIA
-                        // ESTO NO LO HE COMPROBADO TODAVÍA
-                        if ($cursorSql->rowCount() == 1 && $cursorSql[0]==$_POST['pass']) {
-
-                            //si usr y pass son correctos...
-                            session_start();
-                            $_SESSION["usuario"] = $_POST["usr"];
-                            header('Location:pages/homepage.php');                        
-
-                        } else {
-                            echo mensajeError("La contraseña o el usuario no es correcto");
+                        try {
+                            $BD= conexionPDO();
+                            //Sentencia SQL
+                            $sql="SELECT * FROM vendedores WHERE DNI = '".$_POST['usr']."';";
+                            // AL SER USUARIO CLAVE UNICA LA PRIMERA CONDICIÓN ES PRÁCTICAMENTE INNECESARIA
+                            // ESTO NO LO HE COMPROBADO TODAVÍA
+                            $contrasena= hash('sha256', $_POST['pass']);
+                            $tabla= extraerTablas($sql);
+                            if (count($tabla) == 1 && $tabla[0][6]==$contrasena) {
+                                if($tabla[0][5]=='junior'){
+                                    $_SESSION['rol']='junior';
+                                }else{
+                                    $_SESSION['rol']='admin';
+                                }
+                                header('Location: ./pages/homepage.php');
+                            } else {
+                                echo mensajeError("La contraseña o el usuario no es correcto");
+                            }
+                        } catch (Exception $exc) {
+                            echo $exc->getTraceAsString();
                         }
-                    }else{
+                                        }else{
                         echo mensajeError("Formulario no rellenado");
                     }
                 }
             }
-
-            crearBD();
+            comprobarBD();
             ?>
 
             <h1 class="text-center">Inicio de sesión</h1>
@@ -60,12 +63,13 @@
                 <div class="form-group m-2" style="width: 300px">
                     <label for="usr">Usuario</label>
                     <input type="text" class="form-control" id="usr" name="usr" placeholder="Usuario">
+                    <input type="hidden" id="token" name="token" value="<?php echo $_SESSION['token']; ?>"> 
                 </div>
                 <div class="form-group m-2" style="width: 300px">
                     <label for="pass">Contraseña</label>
                     <input type="text" class="form-control" id="pass" name="pass" placeholder="Contraseña">
                 </div>
-                
+                    <!--<input type="hidden" class="form-control" id="token" name="token" value="</*?= $_SESSION['token'] ?*/>">-->
                     <button type="submit" class="btn btn-primary">Iniciar Sesion</button>
             </form>
 
