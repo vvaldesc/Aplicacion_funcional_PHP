b9008172fa8b0ea3785d6d180c07ac07992aa57c
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-27 20:04:09 +0100

    IMPLEMENTANDO PHPMailer (mailDeDni) REQUIRE_ONCE

--- libraries/funciones.php
+++ libraries/funciones.php
@@ -3,12 +3,14 @@
             use PHPMailer\PHPMailer\PHPMailer;
             use PHPMailer\PHPMailer\Exception;
 
-
-            require $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/PHPMailer/Exception.php';
-            require $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/PHPMailer/PHPMailer.php';
-            require $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/PHPMailer/SMTP.php';
+            require_once $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/PHPMailer/Exception.php';
+            require_once $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/PHPMailer/PHPMailer.php';
+            require_once $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/PHPMailer/SMTP.php';
             
-            function mail($mailDeDni){
+            
+            
+            
+            function enviarMail($mailDeDni){
                 
                 try {
                     
@@ -22,7 +24,7 @@
 
 
                     $mail->Username = $mailDeDni; //Tu gmail
-                    $mail->Password = 'Aquí va tu contraseña de aplicación'; //Tu contraseña de aplicación de gamil
+                    $mail->Password = ''; //Tu contraseña de aplicación de gamil
                     $mail->SMTPSecure = 'tls'; //Tipo de seguridad
                     $mail->Port = 587; //Puerto de smtp
                     $mail->Timeout = 5;
@@ -32,7 +34,7 @@
 
                     $mail->addAddress($_POST['email']); //El email que recibe el correo
 
-                    $mail->isHTML(true); //El mensaje enviado es HTML
+                    $mail->isHTML(false); //El mensaje enviado es HTML
 
                     $mail->Subject = "Inicio de sesión con tu cuenta"; //Asunto del mensaje
                     $mail->Body = "Inicio de sesión en concsionario"; //Cuerpo del mensaje                    
