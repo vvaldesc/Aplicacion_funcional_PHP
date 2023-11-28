b19bc5addc49983f0ccbcf2b1f90a9777cd986fb
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-28 18:56:39 +0100

    FUNCION CHECKFORM

--- libraries/funciones.php
+++ libraries/funciones.php
@@ -1,6 +1,52 @@
 <?php
 
 
+/**
+ * DESC:
+ *      First array is supposed to be the POST superglobal variable
+ *      wich has all the form info.
+ *      Second array is supposed to be an indexed array of sting values wich
+ *      form inputs don't have to be completed obligatory.
+ * 
+ *      This function checks if the indicated form inputs are different from "".
+ *      It returns true if all obligatory inputs are completed.
+ * 
+ * @param type $datosForm
+ * @param type $noClave
+ * @return bool
+ */
+function checkForm($datosForm, $noClave = null) {//Recibe dos arrays, (asociativo,indexado)
+    $bandera = true; // I assume that all essential parameters are filled
+
+    $claves = array_keys($datosForm); // Store the keys of the associative array from the form (from the POST superglobal array)
+
+    $i = 0;
+            
+    if (is_array($noClave) && $noClave!=null) { // If non essential array exists
+        while ($bandera && $i < count($datosForm)) { // When $bandera is false, the loop ends, while verifying each form field's content
+            $i2 = 0;
+            while ($bandera && $i2 < count($noClave)) { // 'While' verifying that the form field does not match the array of non-essential form elements
+                if ($claves[$i] != $noClave[$i2]) {
+                    if ($datosForm[$claves[$i]] === "") { // Verification of the content of the form field
+                        $bandera = false;
+                    }
+                }
+                $i2++;
+            }
+            $i++;
+        }
+    } else{ // If essential array non't exists
+        while ($bandera && $i < count($claves)) {
+            if ($datosForm[$claves[$i]]==="") {
+                $bandera = false;
+            }
+            $i++;
+        }
+    }
+    
+    return $bandera;
+}
+
 
 if (!function_exists('enviarMail')) {
 
