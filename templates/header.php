a3b413eb6461ccc8ef2eb7ce96d321b7dc06a800
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-30 15:15:11 +0100

    HEADER FUNCIONAL CON ENLACES DINÁMICOS

--- templates/header.php
+++ templates/header.php
@@ -1,9 +1,35 @@
+
 <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
     <ul class="nav nav-pills">
-        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Inicio</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <?php
+        $enlaces = [
+            'Coches' => 'pages/gestionCoches.php',
+            'Clientes' => 'pages/gestionClientes.php',
+            'Inicio' => 'index.php',
+            'Empleados' => 'pages/gestionEmpleados.php',
+            'Ventas' => 'pages/gestionVentas.php',
+        ];
+        $enlacesFromPages = [
+            'Coches' => 'gestionCoches.php',
+            'Clientes' => 'gestionClientes.php',
+            'Inicio' => '../index.php',
+            'Empleados' => 'gestionEmpleados.php',
+            'Ventas' => 'gestionVentas.php',
+        ];
+
+        // Obtén el nombre del archivo actual sin la ruta
+        $nombreArchivo = basename($_SERVER['PHP_SELF']);
+
+        if ($nombreArchivo!="index.php") {
+            $enlaces=$enlacesFromPages;
+        }
+        unset($enlacesFromPages);
+        
+        // Genera dinámicamente los enlaces
+        foreach ($enlaces as $texto => $ruta) {
+            $claseActiva = ($nombreArchivo === $ruta) ? 'active' : '';
+            echo '<li class="nav-item"><a href="' . $ruta . '" class="nav-link ' . $claseActiva . '">' . $texto . '</a></li>';
+        }
+        ?>
     </ul>
 </header>
