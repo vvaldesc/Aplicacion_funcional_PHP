8430034f68a43b3e985a206c95639bbf0f55684a
vvaldesc <124901115+vvaldesc@users.noreply.github.com>
2023-11-30 21:22:53 +0100

    INDEX REFACTORIZADO

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
     include $_SERVER['DOCUMENT_ROOT'].'/Aplicacion_funcional_PHP/libraries/funciones.php';
     session_start();
     $formError=comprobarCookieInicio($_POST,$_SESSION);
+    if (isset($_POST['guardar'])) {
+        //Aquí entra si hemos accedido a crear la BD
+        crearBD();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -12,25 +16,16 @@
     </head>
     <body>
         <div class="container mt-4">
-            <!-- container -->
             <?php
             generaToken($_SESSION['token'],session_id());
             //comprobar credenciales y token y si no, error
             //el formulario te llevaría a homepage
-            if(isset($_GET['login'])){
-                if($_GET['login']=='false'){
-                    echo mensajeError('No se ha iniciado sesión, o rol aun no asignado.');
-                }
-            }
-            if (isset($_POST['guardar'])) {
-                // Check if the form is submitted
-                crearBD();
+            if ($_GET['login'] ?? false) {
+                echo mensajeError('No se ha iniciado sesión, o rol aún no asignado.');
             }
             ?>
             <h1 class="text-center">Inicio de sesión</h1>
-
             <form method="POST" class="text-center mt-4 p-4 border d-flex align-items-center flex-column" style="width: 400px; margin: auto" action='<?php $_SERVER["PHP_SELF"] ?>'>
-
                 <div class="form-group m-2" style="width: 300px">
                     <label for="usr">Usuario</label>
                     <input value="<?= $formError ? $_POST["usr"] : "" ?>" type="text" class="form-control" id="usr" name="usr" placeholder="Usuario">
@@ -42,13 +37,12 @@
                 </div>
                     <!--<input type="hidden" class="form-control" id="token" name="token" value="</*?= $_SESSION['token'] ?*/>">-->
                     <button type="submit" class="mt-3 btn btn-primary">Iniciar Sesión</button>
-                    </form>
-                    <?php
-                        if (comprobarBD() === false) {
-                            cearBDModal();
-                        }
-                    ?>
-
+            </form>
+            <?php
+                if (comprobarBD() === false) {
+                    cearBDModal();
+                }
+            ?>
         </div>
         <!-- JavaScript y jQuery para habilitar los componentes de Bootstrap -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
